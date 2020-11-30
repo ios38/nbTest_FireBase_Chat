1c779368ed9b76c699e74ff8cc3286566eb2577c
Maksim Romanov <ios38ru@gmail.com>
2020-11-30 20:59:38 +0800

    setup Logout

--- nbTest/ViewController.swift
+++ nbTest/ViewController.swift
@@ -6,12 +6,34 @@
 //
 
 import UIKit
+import RxSwift
+import RxCocoa
+import Firebase
 
 class ViewController: UIViewController {
+    private let disposeBag = DisposeBag()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        let logoutButton: UIButton  = UIButton(type: .system)
+        logoutButton.setTitle("Выйти", for: .normal)
+        logoutButton.titleLabel?.font = .systemFont(ofSize: 18)
+
+        let logoutBarButtonItem = UIBarButtonItem(customView: logoutButton)
+        navigationItem.leftBarButtonItem = logoutBarButtonItem
+        
+        logoutButton.rx.controlEvent(.touchUpInside)
+            .subscribe { [weak self] (_) in
+                //print(">>>>> logout button tapped")
+                do {
+                    try Auth.auth().signOut()
+                    self?.navigationController?.popViewController(animated: true)
+                } catch {
+                    print(error)
+                }
+            }.disposed(by: disposeBag)
+
     }
 
 
